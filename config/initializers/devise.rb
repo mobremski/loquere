bda84b56ee61b8bafdacf0f55f7883de301d3d71
Michael Obremski <mobremski@gmail.com>
2016-07-25 19:44:28 -0400

    added devise key to fix asset pipeline

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -58,7 +58,7 @@ Devise.setup do |config|
   # given strategies, for example, `config.params_authenticatable = [:database]` will
   # enable it only for database (email + password) authentication.
   # config.params_authenticatable = true
-
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
   # Tell if authentication through HTTP Auth is enabled. False by default.
   # It can be set to an array that will enable http authentication only for the
   # given strategies, for example, `config.http_authenticatable = [:database]` will
