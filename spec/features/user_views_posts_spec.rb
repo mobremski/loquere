771d532d4a224aa2f21df4939a29298c9631ade3
Michael Obremski <mobremski@gmail.com>
2016-07-25 10:53:36 -0400

    fixed hound violations

--- spec/features/user_views_posts_spec.rb
+++ spec/features/user_views_posts_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 feature "user sees a list of posts and adds to it" do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:meetup) { FactoryGirl.create(:meetup, user_id: user.id) }
-  let!(:post) { FactoryGirl.create(:post, meetup_id: meetup.id, user_id: user.id)}
+  let!(:post) do
+    FactoryGirl.create(
+      :post,
+      meetup_id: meetup.id,
+      user_id: user.id)
+  end
 
-  scenario "user visits meetup's showpage and sees a list of posts", js: true do
+  scenario "user visits showpage and sees a list of posts", js: true do
     sign_in
     click_link(meetup.name)
 
