ecf9ce08d1ae13858beac752bc935a0dee133ee3
Michael Obremski <mobremski@gmail.com>
2016-07-27 12:47:58 -0400

    fixed hound violations

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Api::PostsController, type: :controller do
-describe "Post API" do
-  before do
-    @user = FactoryGirl.create(:user)
-    allow(controller).to receive(:current_user).and_return(@user)
-    @meetup = FactoryGirl.create(:meetup, user: @user)
-    @post = FactoryGirl.create(:post, user: @user, meetup: @meetup)
-  end
+  describe "Post API" do
+    before do
+      @user = FactoryGirl.create(:user)
+      allow(controller).to receive(:current_user).and_return(@user)
+      @meetup = FactoryGirl.create(:meetup, user: @user)
+      @post = FactoryGirl.create(:post, user: @user, meetup: @meetup)
+    end
 
     it 'gets lists' do
       get :index, meetup_id: @meetup.id
@@ -17,7 +17,7 @@ describe "Post API" do
     end
 
     it "posts posts" do
-      post :create, meetup_id: @post.meetup_id, post: {body: @post.body, meetup_id: @post.meetup_id, user_id: @post.user_id}
+      post :create, meetup_id: @post.meetup_id, post: { body: @post.body, meetup_id: @post.meetup_id, user_id: @post.user_id }
       json = JSON.parse(response.body)
       expect(response).to be_success
       expect(json['post']['body']).to eq(@post.body)
@@ -26,7 +26,6 @@ describe "Post API" do
     it "deletes a post" do
       delete :destroy, meetup_id: @post.meetup_id, id: @post.id
       json = JSON.parse(response.body)
-      binding.pry
       expect(response).to be_success
       expect(json['posts']).to eq(nil)
     end
